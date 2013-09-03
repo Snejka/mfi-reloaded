4f06988d6655fdbf9e7847ebdecced5ef384f652
Nick Ohrn <nohrn@ohrnventures.com>
2013-09-03 16:56:16 -0700

    Implemented the mfi_reloaded_add_image_picker template tag.

--- mfi-reloaded.php
+++ mfi-reloaded.php
@@ -18,6 +18,7 @@ if(!class_exists('MFI_Reloaded')) {
 		//// KEYS
 
 		/// DATA STORAGE
+		private static $image_pickers = array();
 
 		public static function init() {
 			self::add_actions();
@@ -61,6 +62,32 @@ if(!class_exists('MFI_Reloaded')) {
 
 		/// UTILITY
 
+		private static function _normalize_args($args) {
+			$normalized_args = array();
+
+			if(!isset($args['post_types'])) {
+				$normalized_args['post_types'] = array('post', 'page');
+			} else if(!is_array($args['post_types'])) {
+				$normalized_args['post_types'] = array($args['post_types']);
+			}
+
+			$default_labels = array(
+				'name' => __('Featured Image'),
+				'set' => __('Set featured image'),
+				'remove' => __('Remove featured image'),
+				'popup_title' => __('Set Featured Image'),
+				'popup_select' => __('Set featured image'),
+			);
+
+			if(!isset($args['labels']) || !is_array($args['labels'])) {
+				$normalized_args['labels'] = $default_labels;
+			} else {
+				$normalized_args['labels'] = shortcode_atts($default_labels, $args['labels']);
+			}
+
+			return $normalize_args;
+		}
+
 		private static function _redirect($url, $code = 302) {
 			wp_redirect($url, $code); exit;
 		}
@@ -68,6 +95,14 @@ if(!class_exists('MFI_Reloaded')) {
 		//// LINKS
 
 		/// TEMPLATE TAGS
+
+		public static function add_image_picker($name, $args) {
+			if(!is_string($name) || isset(self::$image_pickers[$name])) {
+				return false;
+			}
+
+			self::$image_pickers[$name] = self::_normalize_args($args);
+		}
 
 		public static function get_image_id($name, $post_id) {
 			return false;
